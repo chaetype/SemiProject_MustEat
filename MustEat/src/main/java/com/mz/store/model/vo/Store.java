edaa0782f1576448dab0f3d8b5f2fe333e533dd8
JANGSW94 <psogw0311@hanmail.net>
2022-07-27 15:59:24 +0900

    Add Store.java

---
+++ MustEat/src/main/java/com/mz/store/model/vo/Store.java
@@ -0,0 +1,12 @@
+package com.mz.store.model.vo;
+
+public class Store {
+	
+	private int storeNo;
+	private String localSi;
+	private String localGu;
+	private String localRo;
+	private String storeName;
+	private String storeTag;
+
+}
