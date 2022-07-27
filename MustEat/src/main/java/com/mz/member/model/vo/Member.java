bd1bfb5d6c3df94e3933bdcf8c3d683cdc058b5a
blackngrey <103404826+blackngrey@users.noreply.github.com>
2022-07-27 17:47:42 +0900

    Add Member.java

---
+++ MustEat/src/main/java/com/mz/member/model/vo/Member.java
@@ -0,0 +1,197 @@
+package com.mz.member.model.vo;
+
+import java.sql.Date;
+
+public class Member {
+	
+	private int memNo;
+	private String memId;
+	private String memPwd;
+	private String memName;
+	private String memPhone;
+	private String memEmail;
+	private Date enrollDate;
+	private Date modifyDate;
+	private String memStatus;
+	private String memNickname;
+	private String memGrade;
+	private int addressCode;
+	private String address;
+	private String addressDetail;
+	private String addressRef;
+	private String memImgPath;
+	private String withDraw;
+	
+	public Member() {}
+
+	public Member(int memNo, String memId, String memPwd, String memName, String memPhone, String memEmail,
+			Date enrollDate, Date modifyDate, String memStatus, String memNickname, String memGrade, int addressCode,
+			String address, String addressDetail, String addressRef, String memImgPath, String withDraw) {
+		super();
+		this.memNo = memNo;
+		this.memId = memId;
+		this.memPwd = memPwd;
+		this.memName = memName;
+		this.memPhone = memPhone;
+		this.memEmail = memEmail;
+		this.enrollDate = enrollDate;
+		this.modifyDate = modifyDate;
+		this.memStatus = memStatus;
+		this.memNickname = memNickname;
+		this.memGrade = memGrade;
+		this.addressCode = addressCode;
+		this.address = address;
+		this.addressDetail = addressDetail;
+		this.addressRef = addressRef;
+		this.memImgPath = memImgPath;
+		this.withDraw = withDraw;
+	}
+
+	public int getMemNo() {
+		return memNo;
+	}
+
+	public void setMemNo(int memNo) {
+		this.memNo = memNo;
+	}
+
+	public String getMemId() {
+		return memId;
+	}
+
+	public void setMemId(String memId) {
+		this.memId = memId;
+	}
+
+	public String getMemPwd() {
+		return memPwd;
+	}
+
+	public void setMemPwd(String memPwd) {
+		this.memPwd = memPwd;
+	}
+
+	public String getMemName() {
+		return memName;
+	}
+
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+
+	public String getMemPhone() {
+		return memPhone;
+	}
+
+	public void setMemPhone(String memPhone) {
+		this.memPhone = memPhone;
+	}
+
+	public String getMemEmail() {
+		return memEmail;
+	}
+
+	public void setMemEmail(String memEmail) {
+		this.memEmail = memEmail;
+	}
+
+	public Date getEnrollDate() {
+		return enrollDate;
+	}
+
+	public void setEnrollDate(Date enrollDate) {
+		this.enrollDate = enrollDate;
+	}
+
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+
+	public String getMemStatus() {
+		return memStatus;
+	}
+
+	public void setMemStatus(String memStatus) {
+		this.memStatus = memStatus;
+	}
+
+	public String getMemNickname() {
+		return memNickname;
+	}
+
+	public void setMemNickname(String memNickname) {
+		this.memNickname = memNickname;
+	}
+
+	public String getMemGrade() {
+		return memGrade;
+	}
+
+	public void setMemGrade(String memGrade) {
+		this.memGrade = memGrade;
+	}
+
+	public int getAddressCode() {
+		return addressCode;
+	}
+
+	public void setAddressCode(int addressCode) {
+		this.addressCode = addressCode;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddressDetail() {
+		return addressDetail;
+	}
+
+	public void setAddressDetail(String addressDetail) {
+		this.addressDetail = addressDetail;
+	}
+
+	public String getAddressRef() {
+		return addressRef;
+	}
+
+	public void setAddressRef(String addressRef) {
+		this.addressRef = addressRef;
+	}
+
+	public String getMemImgPath() {
+		return memImgPath;
+	}
+
+	public void setMemImgPath(String memImgPath) {
+		this.memImgPath = memImgPath;
+	}
+
+	public String getWithDraw() {
+		return withDraw;
+	}
+
+	public void setWithDraw(String withDraw) {
+		this.withDraw = withDraw;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [memNo=" + memNo + ", memId=" + memId + ", memPwd=" + memPwd + ", memName=" + memName
+				+ ", memPhone=" + memPhone + ", memEmail=" + memEmail + ", enrollDate=" + enrollDate + ", modifyDate="
+				+ modifyDate + ", memStatus=" + memStatus + ", memNickname=" + memNickname + ", memGrade=" + memGrade
+				+ ", addressCode=" + addressCode + ", address=" + address + ", addressDetail=" + addressDetail
+				+ ", addressRef=" + addressRef + ", memImgPath=" + memImgPath + ", withDraw=" + withDraw + "]";
+	}
+	
+	
+
+}
