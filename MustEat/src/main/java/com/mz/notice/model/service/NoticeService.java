d382a3d9cdae1b198f9159f6bd15da149b399b46
JANGSW94 <psogw0311@hanmail.net>
2022-07-29 17:08:32 +0900

    Update NoticeService.java

--- MustEat/src/main/java/com/mz/notice/model/service/NoticeService.java
+++ MustEat/src/main/java/com/mz/notice/model/service/NoticeService.java
@@ -1,8 +1,30 @@
 package com.mz.notice.model.service;
 
+import static com.mz.common.JDBCTemplate.close;
+import static com.mz.common.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.mz.notice.model.dao.NoticeDao;
+import com.mz.notice.model.vo.Notice;
+import com.mz.notice.model.vo.Tos;
+
 public class NoticeService {
 	
 	// 혜림
 	
 
+	
+	
+	// 서원
+	// 관리자 이용약관 조회
+	public ArrayList<Tos> selectAdminTosList(){
+		Connection conn = getConnection();
+		ArrayList<Tos> list = new NoticeDao().selectAdminTosList(conn);
+		close(conn);
+		return list;
+	}
+	
+	
 }
