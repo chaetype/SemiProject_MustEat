cfe01df482bd289928a1d9028638843d6d290e36
JANGSW94 <psogw0311@hanmail.net>
2022-07-29 16:49:23 +0900

    Update TosAdminListController.java

--- MustEat/src/main/java/com/mz/notice/controller/TosAdminListController.java
+++ MustEat/src/main/java/com/mz/notice/controller/TosAdminListController.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class TosEnrollFormController
  */
 @WebServlet("/toslist.no")
-public class TosEnrollFormController extends HttpServlet {
+public class TosAdminListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public TosEnrollFormController() {
+    public TosAdminListController() {
         super();
         // TODO Auto-generated constructor stub
     }
