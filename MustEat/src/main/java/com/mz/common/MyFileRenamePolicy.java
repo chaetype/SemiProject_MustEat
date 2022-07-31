3aec590888643237e252e98c694ac8a094518c30
blackngrey <choihyelim1004@gmail.com>
2022-07-31 16:40:56 +0900

    Add MyFileRenamePolicy

---
+++ MustEat/src/main/java/com/mz/common/MyFileRenamePolicy.java
@@ -0,0 +1,25 @@
+package com.mz.common;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.oreilly.servlet.multipart.FileRenamePolicy;
+
+public class MyFileRenamePolicy implements FileRenamePolicy {
+
+	@Override
+	public File rename(File originFile) {
+		
+		String originName = originFile.getName();
+		
+		String currentTime = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		int ranNum = (int)(Math.random() * 90000 + 10000);
+		String ext = originName.substring(originName.lastIndexOf("."));
+		
+		String changeName = currentTime + ranNum + ext;
+		
+		return new File(originFile.getParent(), changeName);
+	}
+
+}
