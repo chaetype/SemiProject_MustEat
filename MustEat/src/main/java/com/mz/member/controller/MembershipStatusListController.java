a1cc062fc4397b0e1d7be49a1333d6410372c13e
JANGSW94 <psogw0311@hanmail.net>
2022-08-01 17:33:41 +0900

    Update MembershipStatusListController.java

--- MustEat/src/main/java/com/mz/member/controller/MembershipStatusListController.java
+++ MustEat/src/main/java/com/mz/member/controller/MembershipStatusListController.java
@@ -1,11 +1,16 @@
 package com.mz.member.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.mz.member.model.service.MemberService;
+import com.mz.member.model.vo.Point;
 
 /**
  * Servlet implementation class MembershipStatusListController
@@ -27,7 +32,11 @@ public class MembershipStatusListController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		ArrayList<Point> list = new MemberService().membershipList();
 		
+		request.setAttribute("list", list);
+		
+		request.getRequestDispatcher("views/jsw/membershipAdmin.jsp").forward(request, response);
 		
 	}
 
