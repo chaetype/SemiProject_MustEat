933432aa8c5b728602c54d99e21de5b62fea8e8d
key1018 <103404357+key1018@users.noreply.github.com>
2022-08-07 16:37:04 +0900

    Add

---
+++ MustEat/src/main/java/com/mz/store/controller/SearchCityController.java
@@ -0,0 +1,54 @@
+package com.mz.store.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mz.store.model.service.StoreService;
+import com.mz.store.model.vo.Store;
+
+/**
+ * Servlet implementation class SearchCityController
+ */
+@WebServlet("/searchCity.st")
+public class SearchCityController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchCityController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 메인페이지에서 도시 찾는 Servlet
+		String city = request.getParameter("map");
+		
+		ArrayList<Store> list = new StoreService().searchCity(city);
+		
+		
+		request.setAttribute("list", list);
+		request.getRequestDispatcher("views/common/mainSecondPage.jsp").forward(request, response);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
