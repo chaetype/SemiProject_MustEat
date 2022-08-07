9e3b965104d9cc3b409290ee1747cd84eda74cc5
chaetype <chaeyn07@gmail.com>
2022-08-07 16:55:16 +0900

    수정

--- MustEat/src/main/java/com/mz/store/model/service/StoreService.java
+++ MustEat/src/main/java/com/mz/store/model/service/StoreService.java
@@ -398,4 +398,13 @@ public class StoreService {
 			
 
 		}
+		
+	//채윤 메인페이지 조회	
+	public ArrayList<Store> mainpage(){
+		Connection conn = getConnection();
+		ArrayList<Store> list = new StoreDao().mainpage(conn);
+		
+		close(conn);
+		return list;
+	}
 }
